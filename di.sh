3d6ce4b07301782116f3cf7d01f3060aee929a7b
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2023-03-01 18:45:46 -0500

    Move distributed components in dish

---
+++ di.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+export DISH_TOP=${DISH_TOP:-${BASH_SOURCE%/*}}
+export PASH_TOP=${PASH_TOP:-${DISH_TOP}/pash/}
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/lib/"
+# point to the local downloaded folders
+export PYTHONPATH=${PASH_TOP}/python_pkgs/
+
+"$PASH_TOP/pa.sh" "$@"
