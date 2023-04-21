3201a036ef1b3c593f07895a1cb140db70e54977
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2023-04-21 16:11:16 -0400

    setup docker script

---
+++ scripts/setup-docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -ex
+
+# docker
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt install -y docker-ce
+sudo usermod -aG docker "${USER}"
+
+echo "Don't forget to run" '`sudo su - ${USER}`'
