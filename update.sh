45602564b0ab14633bcb419ec4b0105a5aa8d0bb
Ramiz Dundar <ramizdundar@gmail.com>
2024-05-23 00:54:38 -0400

    Add update script

---
+++ update.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd "$(realpath $(dirname "$0"))"
+
+git pull
+git submodule update
+
+if [[ "$@" == *"--main"* ]]; then
+    pkill -f worker
+    pkill -f discovery
+    pkill -f filereader
+    sleep 2
+    bash /opt/dish/pash/compiler/dspash/worker.sh &> /worker.log &
+else
+    /opt/dish/runtime/scripts/killall.sh
+    sleep 2
+    pkill -f worker
+    sleep 2
+    /opt/dish/docker-hadoop/datanode/run.sh
+fi
