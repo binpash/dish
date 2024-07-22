1179dd10d7dec70a4ba31b4add197d142ec6e2cd
Ramiz Dundar <ramizdundar@gmail.com>
2024-07-22 12:00:16 -0400

    Add go directory to path in build.sh

--- runtime/scripts/build.sh
+++ runtime/scripts/build.sh
@@ -7,6 +7,9 @@ then
     export DISH_TOP=$(realpath $(dirname "$0")/../..)
 fi
 
+# Add go directory to PATH
+export PATH=$PATH:/usr/local/go/bin
+
 cd $DISH_TOP/runtime
 go build -o bin/ dfs/server/filereader_server.go
 go build -o bin/ dfs/client/dfs_split_reader.go
