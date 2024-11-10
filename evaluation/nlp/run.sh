783b610344f82720f8093a3cbd6c463a72fcb574
Ramiz Dundar <ramizdundar@gmail.com>
2024-11-10 16:58:24 -0500

    Reduce nlp wait time to 60

--- evaluation/nlp/run.sh
+++ evaluation/nlp/run.sh
@@ -83,7 +83,7 @@ nlp() {
                 python3 "$DISH_TOP/evaluation/notify_worker.py" resurrect
             fi
 
-            sleep 120
+            sleep 60
         fi
 
         # For every file in output_dir, generate a hash and delete the file
