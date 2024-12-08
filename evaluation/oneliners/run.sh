d405642c7c960782a6ac85e1fa3a487df6d7d3b5
Ramiz Dundar <ramizdundar@gmail.com>
2024-12-08 17:00:42 -0500

    Add microbenchmarks for oneliners

--- evaluation/oneliners/run.sh
+++ evaluation/oneliners/run.sh
@@ -130,5 +130,9 @@ oneliners "dynamic"     "--width 8 --r_split -d $d --distributed_exec --ft dynam
 oneliners "dynamic-m"   "--width 8 --r_split -d $d --distributed_exec --ft dynamic --kill merger"
 oneliners "dynamic-r"   "--width 8 --r_split -d $d --distributed_exec --ft dynamic --kill regular"
 
+# For microbenchmarks
+covid-mts "dynamic-on-m"     "--width 8 --r_split -d $d --distributed_exec --ft dynamic --dynamic_switch_force on --kill merger"
+covid-mts "dynamic-off-m"    "--width 8 --r_split -d $d --distributed_exec --ft dynamic --dynamic_switch_force off --kill merger"
+
 oneliners_hadoopstreaming
 
